4487703d8942e18e5a4d9709b2299f2a177b1ade
FelipeMacedo315 <felipemacedo315@gmail.com>
2023-01-21 15:46:09 -0300

    fazendo a navigation

---
+++ storebooks/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+    <nav>
+      <button @click="showNav = !showNav"></button>
+    </nav>
+    <nav v-show="showNav">
+      <Navigation-Menu />
+    </nav>
+  </main>
+</template>
+<script>
+import NavMenu from "./components/Navigation-Menu.vue";
+export default {
+  components: {
+    "Navigation-Menu": NavMenu,
+  },
+  data() {
+    return {
+      showNav: false,
+    };
+  },
+};
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+  button {
+    padding: 2.5vh;
+    background-image: url("./assets/navigation-bar.png");
+    background-color: transparent;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-repeat: no-repeat;
+    float: left;
+    border: none;
+    border-radius: 10px;
+    &:hover {
+      cursor: pointer;
+      background-color: rgb(172, 169, 169);
+    }
+  }
+}
+</style>
---
+++ storebooks/src/components/Navigation-Menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <li>
+      <router-link to="/userPedidos">Meus pedidos</router-link>
+    </li>
+    <li>
+      <router-link to="/userPedidos">Ofertas</router-link>
+    </li>
+    <li>
+      <router-link to="/userPedidos">Categorias</router-link>
+    </li>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
